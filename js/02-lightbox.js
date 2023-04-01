3985d85b698ee81165b1e0e97fe2e47a96182d98
Vasyl Lepish <117004345+AlessioItaliano@users.noreply.github.com>
2023-04-01 18:46:01 +0300

    second task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,33 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+
+const galleryItemMarkup = galleryItems
+  .map(
+    ({ preview, original, description }) => `
+    <li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`
+  )
+  .join("");
+
+galleryList.insertAdjacentHTML("afterbegin", galleryItemMarkup);
+
+galleryList.addEventListener("click", onClick);
+
+function onClick(evt) {
+  evt.preventDefault();
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  } else {
+    let gallery = new SimpleLightbox(".gallery__link", {
+      captionsData: "alt",
+      captionPosition: "bottom",
+      captionDelay: 250,
+    });
+    gallery.on("show.simplelightbox", function () {});
+  }
+}
