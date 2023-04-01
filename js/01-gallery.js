2a4c78adfa3f361f03aaed3597a5203ab99b90c4
Vasyl Lepish <117004345+AlessioItaliano@users.noreply.github.com>
2023-04-01 11:03:27 +0300

    some

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,31 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+// console.log(galleryItems);
 
-console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+// console.log(galleryList);
+
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) => `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+  )
+  .join("");
+
+// console.log(markup);
+galleryList.insertAdjacentHTML("afterbegin", markup);
+
+galleryList.addEventListener("click", onClick);
+
+function onClick(evt) {
+  evt.preventDefault();
+  console.log(evt.target);
+}
